dc7bf0f4c77d91c7ea9c55f62ea10ba3839516db
Mylena Monte <mylena.monte@liferay.com>
2022-08-05 11:59:57 -0300

    Feat 6 | Scroll Animation

--- script.js
+++ script.js
@@ -78,3 +78,31 @@ function initSmoothScroll() {
 }
 
 initSmoothScroll();
+
+//Scroll Animation
+function initScrollAnimation() {
+  const sections = document.querySelectorAll('.js-scroll');
+  const halfWindow = window.innerHeight * 0.6;
+  if (sections.length) {
+    function animateScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - halfWindow < 0;
+        if (isSectionVisible) {
+          section.classList.add('ativo');
+        }
+
+        /**Caso queira retirar a animação quando o usuário der scroll pra cima
+              else 
+                  section.classList.remove('ativo');
+              **/
+      });
+    }
+
+    animateScroll();
+
+    window.addEventListener('scroll', animateScroll);
+  }
+}
+
+initScrollAnimation();
