2bfd33f62a5821d474c9b294048556750450751d
Mylena Monte <mylena.monte@liferay.com>
2022-08-05 11:56:24 -0300

    Feat 4 | Accordion List

--- script.js
+++ script.js
@@ -21,3 +21,21 @@ function initTabNav() {
 }
 
 initTabNav();
+
+//Accordion List
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const activeClass = 'ativo';
+  if (accordionList.length) {
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
+    });
+  }
+}
+
+initAccordion();
