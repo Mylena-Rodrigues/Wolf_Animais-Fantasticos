9237af2e950721b40e57bfd57610d68a0f31fbb9
Mylena Monte <mylena.monte@liferay.com>
2022-08-18 11:07:56 -0300

    Up 1 | Divide unique js in modules

---
+++ js/modules/scroll-animation.js
@@ -0,0 +1,24 @@
+export default function initScrollAnimation() {
+    const sections = document.querySelectorAll('.js-scroll');
+    const halfWindow = window.innerHeight * 0.6;
+    if (sections.length) {
+      function animateScroll() {
+        sections.forEach((section) => {
+          const sectionTop = section.getBoundingClientRect().top;
+          const isSectionVisible = sectionTop - halfWindow < 0;
+          if (isSectionVisible) {
+            section.classList.add('ativo');
+          }
+  
+          /**Caso queira retirar a animação quando o usuário der scroll pra cima
+                else 
+                    section.classList.remove('ativo');
+                **/
+        });
+      }
+  
+      animateScroll();
+  
+      window.addEventListener('scroll', animateScroll);
+    }
+  }
