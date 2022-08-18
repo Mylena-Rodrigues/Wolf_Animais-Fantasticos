9a1f9dd4b9b4bff2187172544bb2fb9b12953246
Mylena Monte <mylena.monte@liferay.com>
2022-08-18 14:54:21 -0300

    Feat 8 | Tooltip

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@ import initSmoothScroll from './modules/smooth-scroll.js';
 import initScrollAnimation from './modules/scroll-animation.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tab-nav.js';
-import initModal from './modules/modal.js'
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
 
 initSmoothScroll();
 initScrollAnimation();
 initAccordion();
 initTabNav();
 initModal();
+initTooltip();
