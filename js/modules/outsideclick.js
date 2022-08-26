ac5b3065a9e46bbf1e7598df95c0fe71f5d4a8ac
Mylena Monte <mylena.monte@liferay.com>
2022-08-26 15:24:30 -0300

    Feat 9 | Menu Mobile

--- js/modules/outsideclick.js
+++ js/modules/outsideclick.js
@@ -4,14 +4,16 @@ export default function outsideClick(element, userEvents, callback) {
 
   if (!element.hasAttribute(outside)) {
     userEvents.forEach((userEvent) => {
-      html.addEventListener(userEvent, handleOutsideClick);
+      setTimeout(() => {
+        html.addEventListener(userEvent, handleOutsideClick);
+      });
     });
     element.setAttribute(outside, '');
   }
 
   function handleOutsideClick(event) {
     if (!element.contains(event.target)) {
-      element.removeAttribute(outside, '');
+      element.removeAttribute(outside);
       userEvents.forEach((userEvent) => {
         html.removeEventListener(userEvent, handleOutsideClick);
       });
--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ import initTabNav from './modules/tab-nav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropDownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
 
 initSmoothScroll();
 initScrollAnimation();
@@ -13,3 +14,4 @@ initTabNav();
 initModal();
 initTooltip();
 initDropDownMenu();
+initMenuMobile();
