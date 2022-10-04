2586bad25b8a33b83a19aae78ea9a8866771fd08
Mylena Monte <mylena.monte@liferay.com>
2022-10-03 22:30:41 -0300

    Refactor | Feat 3 (Tab Navigation)

--- js/modules/tab-nav.js
+++ js/modules/tab-nav.js
@@ -1,21 +1,29 @@
-export default function initTabNav() {
-  const tabMenu = document.querySelectorAll('.js-tab-menu li');
-  const tabContent = document.querySelectorAll('.js-tab-content section');
-
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove('ativo');
-    });
-    tabContent[index].classList.add('ativo');
+export default class TabNav {
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
+    this.activeClass = 'ativo';
   }
 
-  if (tabMenu.length && tabContent.length) {
-    tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener('click', () => activeTab(index));
+  activeTab(index) {
+    this.tabContent.forEach((section) => {
+      section.classList.remove(this.activeClass);
     });
+    const direcao = this.tabContent[index].dataset.anime;
+    console.log(direcao);
+    this.tabContent[index].classList.add(this.activeClass, direcao);
+  }
 
-    // Caso DEV não possua acesso ao DOM |
-    // Como adicionar a classe ativo assim que a página carregar:
-    // tabContent[0].classList.add('ativo');
+  addTabNavEvent() {
+    this.tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => this.activeTab(index));
+    });
+  }
+
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      this.addTabNavEvent();
+    }
+    return this;
   }
 }
