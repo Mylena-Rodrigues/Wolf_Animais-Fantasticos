056536a155c8f1df1a7cb431f4c935902901b223
Mylena Monte <mylena.monte@liferay.com>
2022-10-04 15:45:32 -0300

    Refactor | Feat 6 (Scroll Animation)

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -1,23 +1,26 @@
-export default function initScrollAnimation() {
-  const sections = document.querySelectorAll('.js-scroll');
-  const halfWindow = window.innerHeight * 0.6;
-  function animateScroll() {
-    sections.forEach((section) => {
+export default class ScrollAnimation {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.halfWindow = window.innerHeight * 0.6;
+    this.animateScroll = this.animateScroll.bind(this);
+  }
+
+  animateScroll() {
+    this.sections.forEach((section) => {
       const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - halfWindow < 0;
+      const isSectionVisible = sectionTop - this.halfWindow < 0;
       if (isSectionVisible) section.classList.add('ativo');
       else if (section.classList.contains('ativo')) {
         section.classList.remove('ativo');
       }
-
-      /* * Caso queira retirar a animação quando o usuário der scroll pra cima
-            else section.classList.remove('ativo');
-      * */
     });
   }
-  if (sections.length) {
-    animateScroll();
 
-    window.addEventListener('scroll', animateScroll);
+  init() {
+    if (this.sections.length) {
+      this.animateScroll();
+      window.addEventListener('scroll', this.animateScroll);
+    }
+    return this;
   }
 }
