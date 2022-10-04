f587a5753285563f0cd1e5af23e0415d88f57786
Mylena Monte <mylena.monte@liferay.com>
2022-10-04 16:35:54 -0300

    Fix | Improving ScrollAnimation perfomance - getDistance, checkDistance and stop methods

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -2,24 +2,42 @@ export default class ScrollAnimation {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.halfWindow = window.innerHeight * 0.6;
-    this.animateScroll = this.animateScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animateScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - this.halfWindow < 0;
-      if (isSectionVisible) section.classList.add('ativo');
-      else if (section.classList.contains('ativo')) {
-        section.classList.remove('ativo');
+  // Pega a distância de cada item com relação ao topo do site
+  getDistance() {
+    this.distances = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.halfWindow),
+      };
+    });
+  }
+
+  // Verifica a distância em cada objeto
+  // Em relação ao scroll do site
+  checkDistance() {
+    this.distances.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
       }
     });
   }
 
+  // Remove o event listener
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
+  }
+
   init() {
     if (this.sections.length) {
-      this.animateScroll();
-      window.addEventListener('scroll', this.animateScroll);
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
     }
     return this;
   }
